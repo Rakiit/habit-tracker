0d7c499d351c07636ac2a9b27d8b7f5ef2918aee
Rakiit <ricardoiarced@gmail.com>
2023-09-28 17:21:16 -0700

    Added the first model and edited the projects view

---
+++ models/project.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "projects.json"
+);
+
+const getProjectsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Project {
+  constructor(name, color, time, person) {
+    this.name = name;
+    this.color = color;
+    this.time = time;
+    this.person = person;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProjectsFromFile((projects) => {
+      projects.push(this);
+      fs.writeFile(p, JSON.stringify(projects), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProjectsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProjectsFromFile((projects) => {
+      const project = projects.find((p) => p.id === id);
+      cb(project);
+    });
+  }
+};
